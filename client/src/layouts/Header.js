b94f92ffaa4ee50d02854456f8cd43d32f6d166c
TrojanVoid <kiroglu.berk@gmail.com>
2024-05-29 03:28:54 +0300

    removed redundant searchbar and notifications dropdown (template relic)

--- client/src/layouts/Header.js
+++ client/src/layouts/Header.js
@@ -104,66 +104,54 @@ export default function Header() {
   };
 
   return (
-    <div className="header-main px-3 px-lg-4">
-      <Link onClick={toggleSidebar} className="menu-link me-3 me-lg-4"><i className="ri-menu-2-fill"></i></Link>
-
-      <div className="form-search me-auto">
-        <input type="text" className="form-control" placeholder="Search" />
-        <i className="ri-search-line"></i>
+    <div className="header-main px-3 px-lg-4 d-flex justify-content-between align-items-center">
+      <div>
+        <Link onClick={toggleSidebar} className="menu-link me-3 me-lg-4"><i className="ri-menu-2-fill"></i></Link>
       </div>
-
-      <Dropdown className="dropdown-skin" align="end">
-        <Dropdown.Toggle as={CustomToggle} onClick={() => skinMode(theme === 'light' ? 'dark' : 'light')}>
-          {theme === 'light' ? <i className="ri-sun-line"></i> : <i className="ri-moon-fill"></i>}
-        </Dropdown.Toggle>
-        <Dropdown.Menu className="mt-10-f">
-          <label>Skin Mode</label>
-          <nav className="nav nav-skin">
-            <Link onClick={() => skinMode('light')} className={theme === 'light' ? "nav-link active" : "nav-link"}>Light</Link>
-            <Link onClick={() => skinMode('dark')} className={theme === 'dark' ? "nav-link active" : "nav-link"}>Dark</Link>
-          </nav>
-          <hr />
-        </Dropdown.Menu>
-      </Dropdown>
-
-      <Dropdown className="dropdown-notification ms-3 ms-xl-4" align="end">
-        <Dropdown.Toggle as={CustomToggle}>
-          <small>3</small><i className="ri-notification-3-line"></i>
-        </Dropdown.Toggle>
-        <Dropdown.Menu className="mt-10-f me--10-f">
-          <div className="dropdown-menu-header">
-            <h6 className="dropdown-menu-title">Notifications</h6>
-          </div>
-          {NotificationList()}
-          <div className="dropdown-menu-footer"><Link to="#">Show all Notifications</Link></div>
-        </Dropdown.Menu>
-      </Dropdown>
-
-      <Dropdown className="dropdown-profile ms-3 ms-xl-4" align="end">
-        <Dropdown.Toggle as={CustomToggle}>
-          <div className="avatar online">
-            <img src={userAvatar} alt="" />
-          </div>
-        </Dropdown.Toggle>
-        <Dropdown.Menu className="mt-10-f">
-          <div className="dropdown-menu-body">
-            <div className="avatar avatar-xl online mb-3"><img src={userAvatar} alt="" /></div>
-            <h5 className="mb-1 text-dark fw-semibold">{user?.username}</h5> {/* Display username */}
-
-            <nav className="nav">
-              <Link to=""><i className="ri-edit-2-line"></i> Edit Profile</Link>
-              <Link to=""><i className="ri-profile-line"></i> View Profile</Link>
+      
+      <div className="d-flex justify-content-center align-items-center">
+      
+        <Dropdown className="dropdown-skin" align="end">
+          <Dropdown.Toggle as={CustomToggle} onClick={() => skinMode(theme === 'light' ? 'dark' : 'light')}>
+            {theme === 'light' ? <i className="ri-sun-line"></i> : <i className="ri-moon-fill"></i>}
+          </Dropdown.Toggle>
+          <Dropdown.Menu className="mt-10-f">
+            <label>Skin Mode</label>
+            <nav className="nav nav-skin">
+              <Link onClick={() => skinMode('light')} className={theme === 'light' ? "nav-link active" : "nav-link"}>Light</Link>
+              <Link onClick={() => skinMode('dark')} className={theme === 'dark' ? "nav-link active" : "nav-link"}>Dark</Link>
             </nav>
             <hr />
-            <nav className="nav">
-              <Link to=""><i className="ri-question-line"></i> Help Center</Link>
-              <Link to=""><i className="ri-lock-line"></i> Privacy Settings</Link>
-              <Link to=""><i className="ri-user-settings-line"></i> Account Settings</Link>
-              <Link to="/pages/signin"><i className="ri-logout-box-r-line"></i> Log Out</Link>
-            </nav>
-          </div>
-        </Dropdown.Menu>
-      </Dropdown>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown className="dropdown-profile ms-3 ms-xl-4" align="end">
+          <Dropdown.Toggle as={CustomToggle}>
+            <div className="avatar online">
+              <img src={userAvatar} alt="" />
+            </div>
+          </Dropdown.Toggle>
+          <Dropdown.Menu className="mt-10-f">
+            <div className="dropdown-menu-body">
+              <div className="avatar avatar-xl online mb-3"><img src={userAvatar} alt="" /></div>
+              <h5 className="mb-1 text-dark fw-semibold">{user?.username}</h5> {/* Display username */}
+
+              <nav className="nav">
+                <Link to=""><i className="ri-edit-2-line"></i> Edit Profile</Link>
+                <Link to=""><i className="ri-profile-line"></i> View Profile</Link>
+              </nav>
+              <hr />
+              <nav className="nav">
+                <Link to=""><i className="ri-question-line"></i> Help Center</Link>
+                <Link to=""><i className="ri-lock-line"></i> Privacy Settings</Link>
+                <Link to=""><i className="ri-user-settings-line"></i> Account Settings</Link>
+                <Link to="/pages/signin"><i className="ri-logout-box-r-line"></i> Log Out</Link>
+              </nav>
+            </div>
+          </Dropdown.Menu>
+        </Dropdown>
+
+      </div>
     </div>
   );
 }
